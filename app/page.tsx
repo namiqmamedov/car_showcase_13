470e56c78ac2787529cda2deb079ca444ac7c646
namiqmamedov <namigmm@code.edu.az>
2023-07-21 17:00:50 +0400

    Updated footer

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,23 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero/>
+
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className="home__text text-container">
+          <h1 className='text-4xl font-extrabold'>
+            Car Catalogue
+          </h1>
+            <p>Explore the cars you might like</p>
+        </div>
+
+        {/* <div className="home__filtes">
+          <SearchBar/>
+
+          <div className="home__filter-container">
+            <CustomFilter/>
+          </div> */}
+        </div>
+      </div>
     </main>
   )
 }
